5ec937fcfa84184f9a01c56c953dd8bd3686ff6d
wendyyanto <yanto.wendy98@gmail.com>
2020-08-22 15:43:29 +0700

    feature: add text, image placeholder and customized reset padding

--- library/src/main/java/dev/wendyyanto/library/LazyLoadComponent.kt
+++ library/src/main/java/dev/wendyyanto/library/LazyLoadComponent.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
+import android.view.ViewGroup
 import android.view.ViewStub
 import android.view.ViewTreeObserver
 import android.view.ViewTreeObserver.OnGlobalLayoutListener
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 
 class LazyLoadComponent @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
@@ -29,7 +32,10 @@ class LazyLoadComponent @JvmOverloads constructor(
     private var screenHeight = -1
     private var parentResId = -1
     private var isInflated = false
+    private var resetPaddingFlag = false
     private lateinit var parentView: View
+    private var imageView: ImageView? = null
+    private var textView: TextView? = null
 
     init {
         orientation = VERTICAL
@@ -40,7 +46,13 @@ class LazyLoadComponent @JvmOverloads constructor(
         try {
             parentResId =
                 typeArray.getResourceId(R.styleable.LazyLoadComponent_parent_layout_id, -1)
+            resetPaddingFlag =
+                typeArray.getBoolean(R.styleable.LazyLoadComponent_reset_padding_on_load, false)
             val layoutRes = typeArray.getResourceId(R.styleable.LazyLoadComponent_layout_res, -1)
+            val imagePlaceholderRes =
+                typeArray.getResourceId(R.styleable.LazyLoadComponent_image_placeholder, -1)
+            val textPlaceHolder =
+                typeArray.getString(R.styleable.LazyLoadComponent_text_placeholder).orEmpty()
             if (layoutRes == -1) {
                 throw IllegalArgumentException(LAYOUT_RESOURCE_IS_INVALID)
             }
@@ -51,6 +63,11 @@ class LazyLoadComponent @JvmOverloads constructor(
                 layoutResource = layoutRes
                 addView(this)
             }
+            if (imagePlaceholderRes != -1) {
+                initPlaceHolderImage(imagePlaceholderRes)
+            } else if (textPlaceHolder.isNotBlank()) {
+                initPlaceholderText(textPlaceHolder)
+            }
         } finally {
             typeArray.recycle()
         }
@@ -58,16 +75,41 @@ class LazyLoadComponent @JvmOverloads constructor(
         setupScrollListener()
     }
 
+    private fun initPlaceholderText(value: String) {
+        textView = TextView(context)
+        textView?.apply {
+            text = value
+            textAlignment = View.TEXT_ALIGNMENT_CENTER
+            addView(this)
+        }
+    }
+
+    private fun initPlaceHolderImage(resource: Int) {
+        imageView = ImageView(context)
+        imageView?.apply {
+            setImageResource(resource)
+            addView(this)
+        }
+    }
+
     private fun setupLayoutListener() {
         this.viewTreeObserver.addOnGlobalLayoutListener(object :
             OnGlobalLayoutListener {
             override fun onGlobalLayout() {
+                setupPlaceholderLayout()
                 getGlobalVisibleRect(componentRect)
                 parentView = rootView.findViewById(parentResId)
                 screenHeight = parentView.measuredHeight
                 viewTreeObserver.removeOnGlobalLayoutListener(this)
             }
         })
+    }
+
+    private fun setupPlaceholderLayout() {
+        imageView?.layoutParams?.width = ViewGroup.LayoutParams.MATCH_PARENT
+        imageView?.layoutParams?.height = ViewGroup.LayoutParams.WRAP_CONTENT
+        textView?.layoutParams?.width = ViewGroup.LayoutParams.MATCH_PARENT
+        textView?.layoutParams?.height = ViewGroup.LayoutParams.WRAP_CONTENT
     }
 
     private fun setupScrollListener() {
@@ -87,9 +129,17 @@ class LazyLoadComponent @JvmOverloads constructor(
 
     private fun loadComponent() {
         if (isInflated) return
-        layoutParams.height = LayoutParams.WRAP_CONTENT
+        resetUI()
         viewStub.inflate()
         isInflated = true
+    }
+
+    private fun resetUI() {
+        layoutParams.height = LayoutParams.WRAP_CONTENT
+        removeView(imageView)
+        removeView(textView)
+        if (!resetPaddingFlag) return
+        setPadding(0, 0, 0, 0)
     }
 
 }
